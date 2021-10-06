77cf3173307685870997f14f01f18bbb79111611
Robert Smart <roca1388@gmail.com>
2021-10-05 22:44:43 -0300

    Formulario para Crear Propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,6 +6,42 @@ incluirTemplates('header');
 <main class="contenedor seccion">
     <h1>Crear</h1>
     <a href="/admin" class="boton boton-verde">Volver</a>
+    <form class="formulario">
+        <fieldset>
+            <legend>Información General</legend>
+            <label for="titulo">Título:</label>
+            <input type="text" id="titulo" placeholder="Título Propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" placeholder="Precio Propiedad">
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="imagen/jpeg, imagen/png">
+
+            <label for="descripcion">Descripción:</label>
+            <textarea id="descripcion"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="Estacionamientos">Estacionamientos:</label>
+            <input type="number" id="Estacionamientos" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+        <fieldset>
+            <legend>Vendedor</legend>
+            <select>
+                <option value="1">Robert</option>
+                <option value="2">Gabriela</option>
+            </select>
+        </fieldset>
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
 </main>
 <?php
 incluirTemplates('footer');
